f5fd09b89cb2951f0437037fbfecb39c83c1e7f6
Tfauves <tfauvelle@gmail.com>
2021-08-25 10:43:34 -0400

    Inital Commit.

---
+++ Car.java
@@ -0,0 +1,45 @@
+package com.company;
+
+
+public class Car extends Vehicle {
+    public Engine carEngine;
+    private String TransmissionType;
+    private boolean isTurboCharged;
+
+
+    public Car(Engine carEngine, String transmissionType, boolean isTurboCharged, String manufacturer, String model, String color, int numberOfPassengers, int maxNumberOfPassengers) {
+        super(manufacturer, model, color, numberOfPassengers, maxNumberOfPassengers);
+        this.carEngine = carEngine;
+        this.TransmissionType = transmissionType;
+        this.isTurboCharged = isTurboCharged;
+    }
+
+    public void start() {
+        carEngine.start();
+    }
+
+    public void stop() {
+        carEngine.stop();
+    }
+
+
+    public void addPassenger(int numberOfPassengers) {
+      // int addedPassenger = getNumberOfPassengers();
+       //int totalPassengers = addedPassenger + numberOfPassengers;
+       setNumberOfPassengers(getNumberOfPassengers() + numberOfPassengers);
+       // setNumberOfPassengers(totalPassengers);
+    }
+
+    public void removePassenger() {
+
+        getNumberOfPassengers();
+    }
+
+//    public String toString() {
+//        String outPut = " | The " + " | " + getColor()
+//    }
+
+
+
+
+}
---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+Challenge for Abstract Classes and Methods:
+
+Create a program focusing on Vehicles
+Required Classes: Vehicle, Engine
+Each Vehicle Class should contain one Engine
+Each Vehicle Class should contain methods to:
+    Start/Stop
+    Add Passengers
+    Remove Passengers
+
+Basic Requirements:
+At least one Abstract Class
+At least two Abstract Methods
+ */
+	// write your code here
+        Engine fourCyl = new Engine(false, "gas");
+
+        Car myCar = new Car(fourCyl, "manual", true, "Chevy", "Sonic", "Black", 1, 4);
+
+
+
+    }
+}
